4c03065ff6061a73d43b7a23c09a94358a6510bc
Chaitra <chaitradangat084@gmail.com>
2019-01-16 00:50:04 +0530

    some code to start the project with ;)

---
+++ AngularJSVintage/Script.js
@@ -0,0 +1,12 @@
+﻿(function () {
+
+    var app = angular.module("app",[]);
+
+    var maincontroller = function ($scope) {
+
+        $scope.message = "Angular Vintage Project using the original AngularJS library :)";
+    };
+
+    app.controller("maincontroller",maincontroller);
+
+}());
