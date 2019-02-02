1f0b86df348c5ccb03eaf8e1f2253af4c9bcd6ef
Chaitra <chaitradangat084@gmail.com>
2019-02-02 19:32:53 +0530

    Search Controller

---
+++ AngularJSVintage/Search.js
@@ -0,0 +1,15 @@
+﻿//The controller for searching user
+
+(function () {
+
+    var app = angular.module("app");
+
+    var SearchController = function($scope) {
+
+        $scope.Search = function () {
+            console.log($scope.username);
+        }
+    }
+
+    app.controller("SearchController", SearchController);
+}());
