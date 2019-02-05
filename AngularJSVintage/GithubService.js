42e9867450c2066933954ff137307a31b1ea5a71
Chaitra <chaitradangat084@gmail.com>
2019-02-05 07:15:13 +0530

    Github service for Github operations

---
+++ AngularJSVintage/GithubService.js
@@ -0,0 +1,30 @@
+﻿(function () {
+
+    var app = angular.module("app");
+
+    var GithubService = function ($http) {
+
+        var GetGithubUserOnComplete = function (response) {
+            console.log(response.data);
+        }
+
+        var GetGithubUserOnError = function (response) {
+            console.log(response.data);
+        }
+
+        var GetGithubUser = function (username) {
+            $http.get('https://api.github.com/users/' + username)
+                .then(GetGithubUserOnComplete, GetGithubUserOnError);
+        }
+
+        return {
+
+            GetGithubUser: GetGithubUser
+
+        };
+    }
+
+
+
+    app.factory("GithubService", GithubService);
+}());
