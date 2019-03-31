918ea7468564644f6424f5db3865b31f799dc588
Chaitra <chaitradangat084@gmail.com>
2019-03-31 17:12:21 +0530

    angular filters

---
+++ AngularJSVintage/GithubFilters.js
@@ -0,0 +1,13 @@
+﻿(function () {
+
+    var app = angular.module("app");
+
+    var UpperCaseCustomFilter = function () {
+        return function (value) {
+            return value.toUpperCase() + ' - converted';
+        }
+    }
+
+    app.filter("UpperCaseCustomFilter",UpperCaseCustomFilter);
+
+}());
