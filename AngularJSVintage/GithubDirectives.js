911d5846311f75cafed593f8af3e55aecd64fee1
Chaitra <chaitradangat084@gmail.com>
2019-04-02 23:57:45 +0530

    added more angular directives

--- AngularJSVintage/GithubDirectives.js
+++ AngularJSVintage/GithubDirectives.js
@@ -2,14 +2,36 @@
 
     var app = angular.module("app");
 
-    //#angular directives should always start lowercase
+    //#follow safe convention that all directives in angular shud be all lower case
+
+    //#region Angular Directive for Rendering html 
     var userdetails = function () {
         return {
-            restrict:"AEC",
-            template:"<b>User Details Template</b>"
+            restrict: "AEC",
+            template: "<b>User Details Template</b>"
         }
     }
+    //#endregion
 
-    app.directive("userdetails",userdetails);
+    //#region Angular Directive for formatting style 
+    var formatstyle = function () {
 
+        var linkfunction = function (scope, element, attributes) {
+
+            var elementToFormat = element.children()[0];
+
+            $(elementToFormat).css({ "background-color": "orange" });
+        };
+
+        return {
+            restrict: "AEC",
+            link: linkfunction
+            //#the return should always be "link" and not "linkfunction"
+        };
+    };
+    //#endregion
+
+    app.directive("userdetails", userdetails);
+
+    app.directive("formatstyle", formatstyle);
 }());
