91af5368c266733dfc677eb58989c3852cbcc2e5
Chaitra <chaitradangat084@gmail.com>
2019-04-02 22:37:16 +0530

    added angular directives

---
+++ AngularJSVintage/GithubDirectives.js
@@ -0,0 +1,15 @@
+﻿(function () {
+
+    var app = angular.module("app");
+
+    //#angular directives should always start lowercase
+    var userdetails = function () {
+        return {
+            restrict:"AEC",
+            template:"<b>User Details Template</b>"
+        }
+    }
+
+    app.directive("userdetails",userdetails);
+
+}());
