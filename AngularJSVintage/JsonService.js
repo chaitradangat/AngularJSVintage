faa5c5e75b19559a04ed5cfd231a348b05dfed46
Chaitra <chaitradangat084@gmail.com>
2019-04-11 07:19:35 +0530

    added fetching methods to JsonService

--- AngularJSVintage/JsonService.js
+++ AngularJSVintage/JsonService.js
@@ -4,10 +4,18 @@
 
     var JsonDataController = function ($scope, $http) {
 
+        var OnJsonDataFetchComplete = function (response) {
+            console.log(response.data);
+        }
+
+        var OnJsonDataFetchError = function () {
+            console.log("error!");
+        }
+
         $scope.GetJsonData = function () {
 
-
-
+            $http.get("/testDatajson.json")
+                .then(OnJsonDataFetchComplete, OnJsonDataFetchError);
 
         }
     }
