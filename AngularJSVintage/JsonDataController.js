5c17c0eca91fba8a5b328138668da1973c882670
Chaitra <chaitradangat084@gmail.com>
2019-04-13 23:36:26 +0530

    json data controller to read data from jsonfile

---
+++ AngularJSVintage/JsonDataController.js
@@ -0,0 +1,28 @@
+﻿(function () {
+
+    var app = angular.module("app");
+
+    var JsonDataController = function ($scope,$resource) {
+
+        var OnJsonDataFetchComplete = function (response) {
+            console.log(response.data);
+        }
+
+        var OnJsonDataFetchError = function () {
+            console.log("error!");
+        }
+
+        $scope.GetJsonData = function () {
+
+            var jsondata = $resource("/testDatajson.json")
+
+            jsondata.get()
+                .then(OnJsonDataFetchComplete, OnJsonDataFetchError);
+
+            //$http.get("/testDatajson.json")
+                //.then(OnJsonDataFetchComplete, OnJsonDataFetchError);
+        }
+    }
+
+    app.controller("JsonDataController", JsonDataController);
+})();
