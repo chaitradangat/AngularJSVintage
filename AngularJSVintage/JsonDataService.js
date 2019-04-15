a2d20a43ffa93a5ca58cca4fbc26a61b52c0fb75
Chaitra <chaitradangat084@gmail.com>
2019-04-15 07:38:56 +0530

    Json Data Service for using across angular application

---
+++ AngularJSVintage/JsonDataService.js
@@ -0,0 +1,37 @@
+﻿(function () {
+
+    var app = angular.module("app");
+
+    var JsonDataServiceOnError = function () {
+
+    };
+
+    var JsonDataServiceOnComplete = function () {
+
+    };
+
+    var FetchJsonData = function () {
+
+    };
+
+
+    var JsonDataService = function ($http) {
+
+        var jsonpath = "testDatajson.json";
+
+        var response = $http.get(jsonpath)
+                            .then(JsonDataServiceOnComplete,JsonDataServiceOnError);
+        return
+        {
+            FetchJsonData:FetchJsonData
+        };
+    };
+
+    app.factory("JsonDataService", JsonDataService);
+
+
+
+
+
+
+})();
