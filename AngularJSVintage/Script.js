d82860fc45f113b7ac2b6fe9ba13ecb1ca9f2ece
Chaitra <chaitradangat084@gmail.com>
2019-04-18 07:22:12 +0530

    changes to angular module definition excluded any external dependencies

--- AngularJSVintage/Script.js
+++ AngularJSVintage/Script.js
@@ -1,7 +1,7 @@
 ﻿(function () {
 
     //The angular module can be created only once but can be declared any number of times. 
-    var app = angular.module("app",[]);
+    var app = angular.module("app", []);
 
     var maincontroller = function ($scope) {
 
