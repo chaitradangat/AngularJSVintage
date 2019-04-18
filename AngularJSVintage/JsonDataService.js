527ae1e6176738c378559ac4a62608e019a571f3
Chaitra <chaitradangat084@gmail.com>
2019-04-18 07:20:55 +0530

    Service to fetch json data.

--- AngularJSVintage/JsonDataService.js
+++ AngularJSVintage/JsonDataService.js
@@ -2,25 +2,28 @@
 
     var app = angular.module("app");
 
-    var JsonDataServiceOnError = function () {
-
-    };
-
-    var JsonDataServiceOnComplete = function () {
-
-    };
-
-    var FetchJsonData = function () {
-
-    };
-
-
     var JsonDataService = function ($http) {
 
         var jsonpath = "testDatajson.json";
 
-        var response = $http.get(jsonpath)
-                            .then(JsonDataServiceOnComplete,JsonDataServiceOnError);
+        var JsonDataServiceOnError = function (reason) {
+            console.log(reason);
+        };
+
+        var JsonDataServiceOnComplete = function (response) {
+
+            console.log(response.data);
+
+            return response.data;
+        };
+
+        var FetchJsonData = function () {
+
+            var response = $http.get(jsonpath)
+                .then(JsonDataServiceOnComplete, JsonDataServiceOnError);
+
+        };
+
         return
         {
             FetchJsonData:FetchJsonData
@@ -28,10 +31,4 @@
     };
 
     app.factory("JsonDataService", JsonDataService);
-
-
-
-
-
-
 })();
